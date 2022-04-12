4e10e2821728501d95020e4db131734eafd9fc2f
AndresChouela <101569013+AndresChouela@users.noreply.github.com>
2022-04-12 08:10:23 -0300

    avance_tp

--- Assets/Scenes/Scripts/ValidarContrasenia.cs
+++ Assets/Scenes/Scripts/ValidarContrasenia.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ValidarContrasenia : MonoBehaviour {
-
+    string contraseniacorrecta;
+    string contraseniausuario;
+    public Text ingresousuario;
 	// Use this for initialization
 	void Start () {
-		
+        contraseniacorrecta = "12345";
 	}
 	
 	// Update is called once per frame
@@ -16,6 +19,14 @@ public class ValidarContrasenia : MonoBehaviour {
 
     public void validarcontrasenia()
     {
-
+        contraseniausuario = ingresousuario.text;
+        if (contraseniacorrecta == contraseniausuario)
+        {
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            Debug.Log("Contraseña incorrecta");
+        }
     }
 }
